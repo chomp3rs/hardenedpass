2c4ae64a7218e1f91a8ff4060d5a4c8eaf021fb5
Gayathri <gayathri6@gatech.edu>
2013-02-20 23:56:45 -0500

    History file update changes

--- src/hardenedpass/AES.java
+++ src/hardenedpass/AES.java
@@ -17,12 +17,10 @@ import sun.misc.BASE64Encoder;
 
 public class AES {
 
-	public static String encrypt(String data, byte[] key)
+	public static String encrypt(String data, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
 	{
-		String strCipherText = new String();
+		    String strCipherText = new String();
 
-
-		try{
 			SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
 			Cipher aesCipher = Cipher.getInstance("AES");
 
@@ -32,92 +30,19 @@ public class AES {
 			byte[] byteDataToEncrypt = data.getBytes();
 			byte[] byteCipherText = aesCipher.doFinal(byteDataToEncrypt); 
 			strCipherText = new BASE64Encoder().encode(byteCipherText);
-			
-			
-/*System.out.println("Helloooo");
-			aesCipher.init(Cipher.DECRYPT_MODE,secretKey);
-			byte[] byteDecryptedText = aesCipher.doFinal(byteCipherText);
-			
-			String strDecryptedText = new String(byteDecryptedText);
-			System.out.println("Decrypted text :" + strDecryptedText);*/
-
-		}
-
-		catch (NoSuchAlgorithmException noSuchAlgo)
-		{
-			System.out.println(" No Such Algorithm exists " + noSuchAlgo);
-		}
-
-		catch (NoSuchPaddingException noSuchPad)
-		{
-			System.out.println(" No Such Padding exists " + noSuchPad);
-		}
-
-		catch (InvalidKeyException invalidKey)
-		{
-			System.out.println(" Invalid Key " + invalidKey);
-		}
-
-		catch (BadPaddingException badPadding)
-		{
-			System.out.println(" Bad Padding " + badPadding);
-		}
-
-		catch (IllegalBlockSizeException illegalBlockSize)
-		{
-			System.out.println(" Illegal Block Size " + illegalBlockSize);
-		}
-
-
-
-		return strCipherText;
+		
+		    return strCipherText;
 	}
 
-	public static String decrypt(String cipherText , byte[] key)
+	public static String decrypt(String cipherText , byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException
 	{
-		String strDecryptedText= new String();
-
-		try{
-			SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
+		    String strDecryptedText= new String();
+        	SecretKeySpec secretKey = new SecretKeySpec(key, "AES");
 			Cipher aesCipher = Cipher.getInstance("AES");
-
 			aesCipher.init(Cipher.DECRYPT_MODE,secretKey);
 			byte[] byteDecryptedText = aesCipher.doFinal(new BASE64Decoder().decodeBuffer(cipherText));
 			strDecryptedText = new String(byteDecryptedText);
-
-		}
-
-		catch (NoSuchAlgorithmException noSuchAlgo)
-		{
-			System.out.println(" No Such Algorithm exists " + noSuchAlgo);
-		}
-
-		catch (NoSuchPaddingException noSuchPad)
-		{
-			System.out.println(" No Such Padding exists " + noSuchPad);
-		}
-
-		catch (InvalidKeyException invalidKey)
-		{
-			System.out.println(" Invalid Key " + invalidKey);
-		}
-
-		catch (BadPaddingException badPadding)
-		{
-			System.out.println(" Bad Padding " + badPadding);
-		}
-
-		catch (IllegalBlockSizeException illegalBlockSize)
-		{
-			System.out.println(" Illegal Block Size " + illegalBlockSize);
-		}
-
-        catch( IOException io)
-        {
-        	System.out.println("IO Exception :" + io.getMessage());
-        }
-
-		return strDecryptedText;
+        	return strDecryptedText;
 	}
 
 
---
+++ src/hardenedpass/HPMath.java
@@ -0,0 +1,35 @@
+package hardenedpass;
+public final class HPMath {
+    /**
+     * Return average value in array, NaN if no such value.
+     */
+    public static double mean(int[] a) {
+        if (a.length == 0) return Double.NaN;
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+        }
+        return sum / a.length;
+    }
+
+     /**
+     * Return sample variance of array, NaN if no such value.
+     */
+    public static double var(int[] a) {
+        if (a.length == 0) return Double.NaN;
+        double avg = mean(a);
+        double sum = 0.0;
+        for (int i = 0; i < a.length; i++) {
+            sum += (a[i] - avg) * (a[i] - avg);
+        }
+        return sum / (a.length - 1);
+    }
+
+        /**
+     * Return sample standard deviation of array, NaN if no such value.
+     */
+    public static double stddev(int[] a) {
+        return Math.sqrt(var(a));
+    }
+
+   }
